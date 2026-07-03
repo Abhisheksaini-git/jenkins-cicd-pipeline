5b07c71933dd3cee2287459235e8f3f9022488a1
Abhisheksaini-git <abhisheksaini93730@gmail.com>
2026-07-03 12:20:30 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/devops/myapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.devops.myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Jenkins CI/CD Pipeline";
+    }
+
+}
